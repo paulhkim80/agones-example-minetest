f6c52c6c82541ae49b0587661688774eeb083863
Paul Hyung Yuel Kim <paulhkim80@gmail.com>
2024-09-05 08:38:28 +0000

    Added Cloud Builder build scripts

--- buildPushToCloudArtifact.sh
+++ buildPushToCloudArtifact.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
-
-# Copyright 2021 Google LLC All Rights Reserved.
+#!/bin/bash
+# 
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#      https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-while true; do
-    /usr/local/bin/minetestserver "$@" 2>&1
-    sleep 10
-done
+# Get the current working directory
+CWD=$(pwd)
+$(chmod ugo+x $CWD/prereq.sh)
+
+#Set Env variables
+. $CWD/prereq.sh
+
+gcloud builds submit --config ./cloudbuild.yaml --machine-type=$BUILD_MACHINE_TYPE .
--- prereq.sh
+++ prereq.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
-
-# Copyright 2021 Google LLC All Rights Reserved.
+#!/bin/bash
+# 
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#      https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-while true; do
-    /usr/local/bin/minetestserver "$@" 2>&1
-    sleep 10
-done
+# Replace with your project name
+export PROJECT_ID="paulsandbox"
+export BUILD_MACHINE_TYPE="e2-highcpu-32"
